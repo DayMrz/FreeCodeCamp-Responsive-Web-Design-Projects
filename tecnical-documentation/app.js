7deee9326d1ea3a135a004fc6cfd9476e792ae1d
Leily <leilymrz@gmail.com>
2022-01-16 14:37:59 -0800

    still styling

---
+++ tecnical-documentation/app.js
@@ -0,0 +1,10 @@
+// var lastId,
+//     navbar = ["#navbar"];
+//     navbarHeight = navbar.outerHeight()+15,
+//     menuItems = navbar.find("a"),
+//     scrollItems = menuItems.map(function(){
+//         var item =$($(this).attr("href"));
+//         if(item.length) {return item;}
+//     });
+
+    
