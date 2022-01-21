cd5261bc04848a2edee8d6e01d8eae7337200eea
Leily <leilymrz@gmail.com>
2022-01-20 18:17:51 -0800

    creating portafolio :3

---
+++ personal-portfolio/js/app.js
@@ -0,0 +1,31 @@
+const $light = document.querySelector('#Light');
+const $dark = document.querySelector('#Dark');
+const $btnTheme = document.querySelector('.Btn-theme');
+
+
+
+$light.addEventListener('click', lightTheme);
+$dark.addEventListener('click', darkTheme);
+
+function lightTheme() {
+    document.body.classList.toggle('light');
+    
+    $dark.hidden = false
+    $light.hidden = true
+}
+
+function darkTheme() {
+    document.body.classList.toggle('dark');
+    // $dark.hidden = true
+    // $light.hidden = false
+}
+
+
+
+// import "./styles.css";
+
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', () => {
+//   document.body.classList.toggle('dark');
+// });
