2060023ce0a1338f3b851a604d729fb7654a3f37
Leily <leilymrz@gmail.com>
2022-02-09 11:50:15 -0800

    organizing

--- personal-portfolio/js/app.js
+++ personal-portfolio/js/app.js
@@ -1,13 +1,13 @@
 function swapStyleSheet(sheet) {
-    document.querySelector("#pagestyle").setAttribute("href", sheet);
+    document.querySelector('#pagestyle').setAttribute('href', sheet);
 }
 
 function initiate() {
-    const printStyle = document.querySelector("#print-style-sheet");
-    const defaultStyle =document.querySelector("#default-style-sheet");
+    const printStyle = document.querySelector('#print-style-sheet');
+    const defaultStyle =document.querySelector('#default-style-sheet');
 
-    printStyle.onclick = function () {swapStyleSheet("./stylesheet/print-view.css")}
-    defaultStyle.onclick = function () {swapStyleSheet("./stylesheet/web-view.css")}
+    printStyle.onclick = function () {swapStyleSheet('stylesheet/print/print-view.css')}
+    defaultStyle.onclick = function () {swapStyleSheet('stylesheet/web/style.css')}
 }
 
 window.onload = initiate;
