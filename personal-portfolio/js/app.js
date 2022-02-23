f936a84c175b639b34160e68fef240e3ce904ec6
Leily <leilymrz@gmail.com>
2022-02-23 14:59:42 -0800

    animation

--- personal-portfolio/js/app.js
+++ personal-portfolio/js/app.js
@@ -1,23 +1,40 @@
-function swapStyleSheet(sheet) {
-    document.querySelector('#pagestyle').setAttribute('href', sheet);
+const barsToggle = document.querySelector('#bars')
+const nav = document.querySelector('.navbar')
+
+
+let showMenu = false
+
+barsToggle.addEventListener('click', toggleMenu);
+
+
+function toggleMenu() {
+  if (!showMenu) {
+    nav.classList.toggle('nav--visible')
+    barsToggle.classList.add('icon-close')
+    barsToggle.classList.remove('icon-bars')
+    showMenu = true
+  } else {
+    nav.classList.remove('nav--visible')
+    barsToggle.classList.add('icon-bars')
+    showMenu = false
+  }
 }
 
-function initiate() {
-    const printStyle = document.querySelector('#print-style-sheet');
-    const defaultStyle =document.querySelector('#default-style-sheet');
-
-    printStyle.onclick = function () {swapStyleSheet('css/print/print-view.css')}
-    defaultStyle.onclick = function () {swapStyleSheet('css/web/style.css')}
+function reveal() {
+  const reveals = document.querySelectorAll('.reveal')
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
 }
 
-window.onload = initiate;
+window.addEventListener("scroll", reveal);
 
-const colorTheme = document.querySelector('#color-theme span')
-const setButtons = document.querySelectorAll('.icon-btn-set')
 
-colorTheme.addEventListener('click', lightTheme)
-
-function lightTheme() {
-        console.log('keep')
-    document.body.classList.toggle('light')
-}
